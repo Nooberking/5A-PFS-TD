3d38f82162d5e6de8fa1e899cacb807f1608e2cf
David Manuel (PC Polytech) <davidmbmanuel@gmail.com>
2022-10-02 10:24:39 +0200

    Adding Admin reservation controller and cleaning ReservationService

--- covid-api/src/main/java/fr/polytech/covid/service/ReservationService.java
+++ covid-api/src/main/java/fr/polytech/covid/service/ReservationService.java
@@ -3,7 +3,6 @@ package fr.polytech.covid.service;
 import fr.polytech.covid.entity.Center;
 import fr.polytech.covid.entity.Patient;
 import fr.polytech.covid.entity.Reservation;
-import fr.polytech.covid.repository.CenterRepository;
 import fr.polytech.covid.repository.PatientRepository;
 import fr.polytech.covid.repository.ReservationRepository;
 import org.springframework.stereotype.Service;
@@ -15,18 +14,13 @@ import java.util.List;
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
-    private final CenterRepository centerRepository;
     private final PatientRepository patientRepository;
 
-    public ReservationService(ReservationRepository reservationRepository, CenterRepository centerRepository, PatientRepository patientRepository) {
+    public ReservationService(ReservationRepository reservationRepository, PatientRepository patientRepository) {
         this.reservationRepository = reservationRepository;
-        this.centerRepository = centerRepository;
         this.patientRepository = patientRepository;
     }
 
-    public Reservation getReservation(int id){
-        return reservationRepository.findById(id).orElse(null);
-    }
     public Reservation getReservationOfPatient(int patient_id){
         Patient patient = patientRepository.findById(patient_id).orElse(null);
         if(patient != null) return getReservationOfPatient(patient);
@@ -42,11 +36,6 @@ public class ReservationService {
         return reservations;
     }
 
-    public List<Reservation> reservations(int center_id){
-        Center center = centerRepository.findById(center_id).orElse(null);
-        if(center != null) return reservations(center);
-        else return null;
-    }
     public List<Reservation> reservations(Center center){
         return new ArrayList<>(reservationRepository.findByCenter(center));
     }
@@ -57,8 +46,5 @@ public class ReservationService {
         reservationRepository.delete(reservation);
     }
 
-    public void deleteReservation(int id){
-        reservationRepository.deleteById(id);
-    }
 
 }
