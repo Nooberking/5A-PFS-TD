3639d63984e66638844f39dd7bdd9bc2c51ee081
David Manuel (PC Polytech) <davidmbmanuel@gmail.com>
2022-10-02 10:11:24 +0200

    Adding Admin patient controller and cleaning PatientService

---
+++ covid-api/src/main/java/fr/polytech/covid/controller/AdminPatientController.java
@@ -0,0 +1,27 @@
+package fr.polytech.covid.controller;
+
+import fr.polytech.covid.entity.Patient;
+import fr.polytech.covid.service.PatientService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("admin")
+public class AdminPatientController {
+
+    private final PatientService patientService;
+
+    public AdminPatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+    @GetMapping("/patients")
+    public List<Patient> getPatientsByName(@RequestParam String last_name){
+        return patientService.getPatients(last_name);
+    }
+
+    @PutMapping("/patient")
+    public void confirmVaccination(@RequestBody Patient patient){
+        patientService.confirmVaccination(patient);
+    }
+}
