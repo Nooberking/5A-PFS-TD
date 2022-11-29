38d68b518341cce1421b884b8e01cd938e1497be
David Manuel (PC Polytech) <davidmbmanuel@gmail.com>
2022-11-29 13:59:36 +0100

    creating roles + more employees infos

--- covid-api/src/main/java/fr/polytech/covid/entity/Employee.java
+++ covid-api/src/main/java/fr/polytech/covid/entity/Employee.java
@@ -1,8 +1,7 @@
 package fr.polytech.covid.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 public class Employee {
@@ -12,14 +11,30 @@ public class Employee {
     private int id;
 
     private String username;
-
+    private String firstName;
+    private String lastName;
+    @ManyToOne
+    @JoinColumn(name="center_id")
+    private Center center;
     private String password;
+
+    @ManyToMany
+    @JoinTable(
+            name = "employees_role",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
 
     public Employee() {
     }
 
-    public Employee(String username, String password) {
+    public Employee(String username,String firstName, String lastName, Center center, String password) {
         this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.center = center;
         this.password = password;
     }
 
@@ -33,6 +48,38 @@ public class Employee {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Center getCenter() {
+        return center;
+    }
+
+    public void setCenter(Center center) {
+        this.center = center;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
     }
 
     public String getPassword() {
--- covid-api/src/main/java/fr/polytech/covid/service/EmployeeService.java
+++ covid-api/src/main/java/fr/polytech/covid/service/EmployeeService.java
@@ -1,10 +1,15 @@
 package fr.polytech.covid.service;
 
+import fr.polytech.covid.entity.Center;
 import fr.polytech.covid.entity.Employee;
+import fr.polytech.covid.entity.Role;
+import fr.polytech.covid.repository.CenterRepository;
 import fr.polytech.covid.repository.EmployeeRepository;
+import fr.polytech.covid.repository.RoleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,11 +26,19 @@ import java.util.Optional;
 public class EmployeeService implements UserDetailsService {
     private static Logger log = LoggerFactory.getLogger(EmployeeService.class);
     private final EmployeeRepository employeeRepository;
+    private final RoleRepository roleRepository;
+    private final CenterRepository centerRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder) {
+    public EmployeeService(
+            EmployeeRepository employeeRepository,
+            RoleRepository roleRepository,
+            CenterRepository centerRepository,
+            PasswordEncoder passwordEncoder) {
         this.employeeRepository = employeeRepository;
+        this.roleRepository = roleRepository;
+        this.centerRepository = centerRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -32,11 +46,48 @@ public class EmployeeService implements UserDetailsService {
     public void createUserDefault() {
         if (employeeRepository.findByUsername("user").isEmpty()) {
             log.info("Création de l'employé par defaut");
-            Employee employee = new Employee();
-            employee.setUsername("user");
-            employee.setPassword(passwordEncoder.encode("password"));
-            this.employeeRepository.save(employee);
+            Role doctorRole = roleRepository.findByName("DOCTOR");
+            Center center = centerRepository.findByNameContainsIgnoreCaseOrderByNameAsc("Centre de test").get(0);
+            createEmployee("user", "user", "test", doctorRole, "password", center);
         }
+    }
+    @PostConstruct
+    public void createAdminDefault(){
+        if (employeeRepository.findByUsername("admin").isEmpty()) {
+            log.info("Création de l'admin par défaut");
+            Role adminRole = roleRepository.findByName("ADMIN");
+            Center center = centerRepository.findByNameContainsIgnoreCaseOrderByNameAsc("Centre de test").get(0);
+            createEmployee("admin", "admin", "test", adminRole, "password", center);
+        }
+    }
+
+    @PostConstruct
+    public void createSuperAdminDefault() {
+        if(employeeRepository.findByUsername("super_admin").isEmpty()){
+            log.info("Création du super admin par défaut");
+            Employee superAdmin = new Employee();
+            Role superAdminRole = roleRepository.findByName("SUPER_ADMIN");
+            createEmployee("super_admin","super_admin","test",superAdminRole,"password",null);
+        }
+    }
+
+    public void createEmployee(
+            String username,
+            String firstName,
+            String lastName,
+            Role role,
+            String password,
+            @Nullable Center center){
+        Employee employee = new Employee();
+        employee.setUsername(username);
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setRoles(Collections.singletonList(role));
+        employee.setPassword(passwordEncoder.encode(password));
+        employee.setCenter(center);
+        this.employeeRepository.save(employee);
+
+
     }
 
 
